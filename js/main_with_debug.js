9e049a49a1733975ccef875119ef66887da6895d
Ramagaiver <chanodomtannarat@gmail.com>
2026-02-02 23:27:04 -0600

    Finished Activity 3

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -7,6 +7,7 @@ secondid.addEventListener("click", function() {
     counter = counter + 1;
 })
 
+// Initializes citiesTable, will be called later
 function initialize(){
 	cititesTable();
 }
@@ -44,20 +45,23 @@ function cititesTable(){
 
     // Create header row
     var headerRow = document.createElement("tr");
+	table.appendChild(headerRow);
 	console.log('Created header row');
 
+	// City and Population header row
+	headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>")
 
 	// Loop to add a new row for each city
 	for (let i=0; i < cityPop.length; i++) {
         // Assign html strings to a variable
-        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td><tr>";
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
         
         // Add the row's html string to the table
         table.insertAdjacentHTML('beforeend', rowHtml);
 		console.log(`Added row ${rowHtml}`);
 	};
 
-    // Pushes the table to cityTable
+    // Pushes the table to cityTable div
     document.querySelector("#cityTable").appendChild(table);
 	console.log('Pushed table to #cityTable')
 
@@ -68,86 +72,73 @@ function cititesTable(){
 	console.log('Calling addEvents')
 }
 
-function addColumns(cipyPop){
+// Function that will add additional rows to each column
+function addColumns(cityPop){
+
+	// All rows
 	var rows = document.querySelectorAll("tr")
 
-	for (let i=0; i < rows.length; i++){
+	// This for loop iterates and creates a new column for each row
+	for (let i=0; i < cityPop.length + 1; i++){
+		// Since there is i citySize rows, i==0 represents the City Size header
 		if (i==0) {
-			rows.insertAdjacentHTML('beforeend', '<th>City Size,/th.');
+			rows[i].insertAdjacentHTML('beforeend', '<th>City Size</th>');
 		} else {
 			var citySize;
 
+			// We do i-1 to remove the additional row created by the header
 			if (cityPop[i-1].population < 100000){
 				citySize = 'Small'
 			} else if (cityPop[i-1].population < 500000){
 				citySize = 'Medium'
 			} else {
-				citySiz = 'Large'
+				citySize = 'Large'
 			};
 
-			rows.insertAdjacentHTML('beforeend', '<td>' + citySize +  '</td>');
+			// Adds the citySize rows
+			rows[i].insertAdjacentHTML('beforeend', '<td>' + citySize +  '</td>');
 		};
 	};
 };
 
-function initialize(){
-	cititesTable();
+// Handles event listeners for cities table
+function addEvents(){
+
+	// Gets the table element
+	table = document.querySelector("table");
+
+	// Picks a random color when the "mouseover" event is called
+	document.querySelector("table").addEventListener("mouseover", function(){
+		
+		var color = "rgb(";
+
+		for (var i=0; i<3; i++){
+
+			var random = Math.round(Math.random() * 255);
+
+			// This was previously set to a string, so it didn't actually do anything
+			// color += "random";
+			color += random;
+
+			if (i<2){
+				color += ",";
+			
+			} else {
+				color += ")";
+		};
+		// Sets the table's color
+		table.style.color = color;
+	}});
+
+	// Alert when clicking on the cities table
+	function clickme(){
+
+		alert('Hey, you clicked me!');
+	};
+
+	// Event listener for clicking on the cities table
+	document.querySelector("table").addEventListener("click", clickme)
 };
 
-// function addColumns(cityPop){
-    
-//     document.querySelectorAll("tr").forEach(function(row, i){
-
-//     	if (i == 0){
-
-//     		row.insertAdjacntHTML('beforeend', '<th>City Size</th>');
-//     	} else {
-
-//     		var citySize;
-
-//     		if (cityPop[i-1].population < 100000){
-//     			citySize = 'Small';
-
-//     		} else if (cityPop[i-1].population < 500000){
-//     			citysize = 'Medium';
-
-//     		} else {
-//     			citySize = 'Large';
-//     		};
-
-// 			row.insertAdjacntHTML = '<td' + citySize + '</td>';
-//     	};
-//     });
-// };
-
-// function addEvents(){
-
-// 	document.querySelector("table").addEventListener("mouseover", function(){
-		
-// 		var color = "rgb(";
-
-// 		for (var i=0; i<3; i++){
-
-// 			var random = Math.round(Math.random() * 255);
-
-// 			color += "random";
-
-// 			if (i<2){
-// 				color += ",";
-			
-// 			} else {
-// 				color += ")";
-// 		};
-
-// 		document.querySelector("table").color = color;
-// 	});
-
-// 	function clickme(){
-
-// 		alert('Hey, you clicked me!');
-// 	};
-
-// 	document.querySelector("table").addEventListener("click", clickme)
-// };
-
+// Calls citiesTable
 initialize();
